256084499ed5dea945e917a788ccd637059a3e19
Yerson Roa <yerson132@gmail.com>
2018-01-22 19:20:05 +0100

    Added jupyterhub archive.

--- Plugins/ckanext-ggauth/ckanext/ggauth/logic/action/update.py
+++
@@ -1,8 +0,0 @@
-	
-def permission_ask(context, data_dict=None):
-	# Data dict will be something like this
-    '''{'auth.can_download': u'', 'auth.can_metada': u'', 'user': u'ggcatu', 'resource_id': u'b7f4f095-203a-4a28-bfb1-0d7433bdc948'}'''
-
-    # We would need to update the database,
-    # Maybe we don't need any return
-    return data_dict
--- Plugins/ckanext-ggauth/run_test.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-export PYTHONWARNINGS="ignore"
-source $CKAN_HOME/bin/activate
-nosetests --ckan -v -s --with-pylons=$CKAN_CONFIG/ckan_test.ini --nologcapture ckanext/ggauth/tests/test_utils.py
